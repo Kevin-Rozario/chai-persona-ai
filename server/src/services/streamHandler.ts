40df9fdcc7135712a80768c3d074f2cde9d43dc2
Kevin Rozario <rozariokevin73@gmail.com>
2026-07-05 14:57:16 +0530

    fix: modify stream handler

---
+++ server/src/services/streamHandler.ts
@@ -0,0 +1,17 @@
+import type { Response } from "express";
+
+export function initSSE(res: Response) {
+  res.setHeader("Content-Type", "text/event-stream");
+  res.setHeader("Cache-Control", "no-cache");
+  res.setHeader("Connection", "keep-alive");
+  res.flushHeaders();
+}
+
+export function writeSSEEvent(res: Response, event: string, data: unknown) {
+  res.write(`event: ${event}\n`);
+  res.write(`data: ${JSON.stringify(data)}\n\n`);
+}
+
+export function endSSE(res: Response) {
+  res.end();
+}
